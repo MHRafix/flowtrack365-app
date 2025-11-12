daa63503e40853dd08a7f9abeb1a0abeda720c16
MHRafix <fmhrafiz27@gmail.com>
2025-11-12 11:55:03 +0600

    organization layout done

---
+++ src/components/layouts/OrganizationLayout.tsx
@@ -0,0 +1,48 @@
+import { StorageUtil } from '@/lib/storage.util';
+import { fetchME } from '@/store/auth.atom';
+import { useRouter } from '@tanstack/react-router';
+import { LogOutIcon } from 'lucide-react';
+import React, { PropsWithChildren } from 'react';
+import { useAppConfirm } from '../AppConfirm';
+import { ModeToggler } from '../ModeToggler';
+import { Button } from '../ui/button';
+
+const OrganizationLayout: React.FC<PropsWithChildren> = ({ children }) => {
+	const appConfirmHandle = useAppConfirm();
+	const router = useRouter();
+	return (
+		<>
+			<header className='dark:bg-[#172031] bg-[#FBFBFB] border-b !py-8 flex h-20 shrink-0 justify-between items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 px-4'>
+				<div className='container mx-auto w-full flex justify-between pt-0 items-center gap-2'>
+					<h1 className='text-xl md:text-2xl font-semibold'>FlowTrack365</h1>{' '}
+					<div className='flex items-center gap-2 md:gap-5'>
+						<ModeToggler />{' '}
+						<Button
+							variant={'outline'}
+							onClick={() => {
+								appConfirmHandle.show({
+									title: 'Logout',
+									onConfirm: async () => {
+										StorageUtil.removeItem('token');
+										StorageUtil.removeItem('orgUID');
+										await fetchME();
+										router.invalidate();
+									},
+								});
+							}}
+							className='!cursor-pointer text-red-500 hover:text-red-500'
+						>
+							<LogOutIcon />
+							Logout
+						</Button>
+					</div>
+				</div>
+			</header>
+			<div className='flex flex-col flex-1 p-4 gap-4'>
+				<>{children}</>
+			</div>
+		</>
+	);
+};
+
+export default OrganizationLayout;
--- src/pages/organizations/index.tsx
+++ src/pages/organizations/index.tsx
@@ -1,3 +1,4 @@
+import OrganizationLayout from '@/components/layouts/OrganizationLayout';
 import { Skeleton } from '@/components/ui/skeleton';
 import { gqlRequest } from '@/lib/api-client';
 import { getFileUrl } from '@/lib/getFileUrl';
@@ -43,69 +44,74 @@ function RouteComponent() {
 			}),
 	});
 	return (
-		<div className='flex sm:h-screen items-center justify-center bg-white dark:bg-slate-900 my-5'>
-			<div className='w-full px-5 lg:px-0 lg:w-10/12 xl:w-9/12'>
-				<h2 className='text-2xl font-semibold my-5'>Select Organizations</h2>
-				<div className='w-full bg-slate-100 dark:bg-slate-800 p-6 rounded-sm'>
-					<div className='w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 items-stretch'>
-						<Link to='/organizations/create-organization'>
-							<div className='!h-[200px] sm:w-full  sm:h-full cursor-pointer dark:bg-slate-900 bg-slate-200 hover:bg-slate-200 hover:dark:bg-slate-900 hover:duration-500 rounded-md flex justify-center items-center'>
-								<Plus size={40} />
-							</div>
-						</Link>
-						{isLoading ? (
-							<>
-								{new Array(7).fill(7).map((_, idx) => (
-									<Skeleton
-										key={idx}
-										className='h-[200px] bg-slate-200 dark:bg-slate-900'
-									/>
-								))}
-							</>
-						) : (
-							<>
-								{data?.myOrganizations?.nodes?.map(
-									(organization, idx: number) => (
-										<Link
+		<OrganizationLayout>
+			<div className='flex sm:h-screen items-center justify-center bg-white dark:bg-slate-900 my-5'>
+				<div className='w-full lg:px-0 container mx-auto'>
+					<h2 className='text-2xl font-semibold my-5'>Select Organizations</h2>
+					<div className='w-full bg-slate-100 dark:bg-slate-800 p-6 rounded-sm'>
+						<div className='w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 items-stretch'>
+							<Link to='/organizations/create-organization'>
+								<div className='!h-[200px] sm:w-full  sm:h-full cursor-pointer dark:bg-slate-900 bg-slate-200 hover:bg-slate-200 hover:dark:bg-slate-900 hover:duration-500 rounded-md flex justify-center items-center'>
+									<Plus size={40} />
+								</div>
+							</Link>
+							{isLoading ? (
+								<>
+									{new Array(7).fill(7).map((_, idx) => (
+										<Skeleton
 											key={idx}
-											to={'/organizations/$orgId'}
-											params={{ orgId: organization?.orgUID }}
-										>
-											<div
-												className='h-[200px] sm:h-full cursor-pointer dark:bg-slate-800 bg-slate-100 hover:dark:bg-slate-900 hover:bg-slate-200 hover:duration-300 rounded-md px-2 py-4 border'
-												onClick={() => {
-													localStorage.setItem('orgUID', organization?.orgUID);
-													// jotaiStore.set(userAtom, (draft) => {
-													// 	draft.orgUID = organization?.orgUID;
-													// });
-												}}
+											className='h-[200px] bg-slate-200 dark:bg-slate-900'
+										/>
+									))}
+								</>
+							) : (
+								<>
+									{data?.myOrganizations?.nodes?.map(
+										(organization, idx: number) => (
+											<Link
+												key={idx}
+												to={'/organizations/$orgId'}
+												params={{ orgId: organization?.orgUID }}
 											>
-												<img
-													src={
-														getFileUrl(organization?.Logo) ||
-														'https://cdn-icons-png.flaticon.com/128/12192/12192153.png'
-													}
-													alt='organization icon'
-													width={80}
-													height={80}
-													className='mx-auto'
-												/>
+												<div
+													className='h-[200px] sm:h-full cursor-pointer dark:bg-slate-800 bg-slate-100 hover:dark:bg-slate-900 hover:bg-slate-200 hover:duration-300 rounded-md px-2 py-4 border'
+													onClick={() => {
+														localStorage.setItem(
+															'orgUID',
+															organization?.orgUID
+														);
+														// jotaiStore.set(userAtom, (draft) => {
+														// 	draft.orgUID = organization?.orgUID;
+														// });
+													}}
+												>
+													<img
+														src={
+															getFileUrl(organization?.Logo) ||
+															'https://cdn-icons-png.flaticon.com/128/12192/12192153.png'
+														}
+														alt='organization icon'
+														width={80}
+														height={80}
+														className='mx-auto'
+													/>
 
-												<h2 className='text-lg text-center my-2 font-bold'>
-													{organization?.name || 'N/A'}
-												</h2>
-												<p className='text-center my-2 font-medium'>
-													{organization?.orgUID}
-												</p>
-											</div>
-										</Link>
-									)
-								)}
-							</>
-						)}
+													<h2 className='text-lg text-center my-2 font-bold'>
+														{organization?.name || 'N/A'}
+													</h2>
+													<p className='text-center my-2 font-medium'>
+														{organization?.orgUID}
+													</p>
+												</div>
+											</Link>
+										)
+									)}
+								</>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</OrganizationLayout>
 	);
 }
